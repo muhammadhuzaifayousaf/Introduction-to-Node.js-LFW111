466ed8093a374b41a2e6931851d8f50b4d26f12e
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-09-04 19:34:19 +0500

    Updating data-utils.js for Lesson 04 Enhancing an HTTP Server with WebSockets (7)

--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -1,21 +1,24 @@
 "use strict";
 import fp from "fastify-plugin";
-import {promisify} from "node:util"
+import { promisify } from "node:util";
 
 // Promisify setTimeout
 const timeout = promisify(setTimeout);
 
+// Mock data
 const orders = {
   A1: { total: 3 },
   A2: { total: 7 },
   B1: { total: 101 },
 };
 
+// Map category to ID prefix
 const catToPrefix = {
   electronics: "A",
   confectionery: "B",
 };
 
+// Simulate realtime orders
 async function* realtimeOrdersSimulator() {
   const ids = Object.keys(orders);
   while (true) {
@@ -25,30 +28,34 @@ async function* realtimeOrdersSimulator() {
     const { total } = orders[id];
     yield JSON.stringify({ id, total });
     await timeout(1500);
-  }
+ }
 }
 
+// Return current orders
 function* currentOrders(category) {
   const idPrefix = catToPrefix[category];
   if (!idPrefix) return;
   const ids = Object.keys(orders).filter((id) => id[0] === idPrefix);
-  for (const id of ids)
+  for (const id of ids) {
     yield JSON.stringify({ id, ...orders[id] });
   }
+}
 
-  const calculateID = (idPrefix, data) => {
-    const sorted = [...new Set(data.map(({ id }) => id))];
-    const next = Number(sorted.pop().slice(1)) + 1;
-    return `${idPrefix}${next}`
-  };
-  
-  export default fp(async function (fastify, opts) {
-    fastify.decorate("currentOrders", currentOrders);
-    fastify.decorate("realtimeOrders", realtimeOrdersSimulator);
-    fastify.decorate("mockDataInsert", function (request, category, data) {
-      const idPrefix = catToPrefix[category];
-      const id = calculateID(idPrefix, data);
-      data.push({ id, ...request.body });
-      return data;
-    });
+// Calculate next ID
+const calculateID = (idPrefix, data) => {
+  const sorted = [...new Set(data.map(({ id }) => id))];
+  const next = Number(sorted.pop().slice(1)) + 1;
+  return `${idPrefix}${next}`;
+};
+
+// Plugin
+export default fp(async function (fastify, opts) {
+  fastify.decorate("currentOrders", currentOrders);
+  fastify.decorate("realtimeOrders", realtimeOrdersSimulator);
+  fastify.decorate("mockDataInsert", function (request, category, data) {
+    const idPrefix = catToPrefix[category];
+    const id = calculateID(idPrefix, data);
+    data.push({ id, ...request.body });
+    return data;
   });
+});
