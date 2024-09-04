270cb60f04100868b21941978007a96935bf528e
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-09-04 19:30:39 +0500

    Updating data-utils.js for Lesson 04 Enhancing an HTTP Server with WebSockets (5)

--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -15,3 +15,23 @@ const catToPrefix = {
   electronics: "A",
   confectionery: "B",
 };
+
+async function* realtimeOrdersSimulator() {
+  const ids = Object.keys(orders);
+  while (true) {
+    const delta = Math.floor(Math.random() * 7) + 1;
+    const id = ids[Math.floor(Math.random() * ids.length)];
+    orders[id].total += delta;
+    const { total } = orders[id];
+    yield JSON.stringify({ id, total });
+    await timeout(1500);
+  }
+}
+
+function* currentOrders(category) {
+  const idPrefix = catToPrefix[category];
+  if (!idPrefix) return;
+  const ids = Object.keys(orders).filter((id) => id[0] === idPrefix);
+  for (const id of ids)
+    yield JSON.stringify({ id, ...orders[id] });
+  }
