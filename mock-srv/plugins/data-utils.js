01355862a2d2f6d09533581dcea8cf94807c9cee
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-09-04 19:25:02 +0500

    Updating data-utils.js for Lesson 04 Enhancing an HTTP Server with WebSockets (4)

--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -1,23 +1,17 @@
 "use strict";
-
 import fp from "fastify-plugin";
+import {promisify} from "node:util"
+
+// Promisify setTimeout
+const timeout = promisify(setTimeout);
+
+const orders = {
+  A1: { total: 3 },
+  A2: { total: 7 },
+  B1: { total: 101 },
+};
 
 const catToPrefix = {
   electronics: "A",
   confectionery: "B",
 };
-
-const calculateID = (idPrefix, data) => {
-  const sorted = [...new Set(data.map(({ id }) => id))];
-  const next = Number(sorted.pop().slice(1)) + 1;
-  return `${idPrefix}${next}`;
-};
-
-export default fp(async function (fastify, opts) {
-  fastify.decorate("mockDataInsert", function (request, category, data) {
-    const idPrefix = catToPrefix[category];
-    const id = calculateID(idPrefix, data);
-    data.push({ id, ...request.body });
-    return data
-  });
-});
