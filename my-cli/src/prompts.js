734de4f6edf652bee835d38dbe376488e2aa89c5
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-09-09 23:48:03 +0500

    Updating prompts.js Lesson 05 Adding Interactivity (3)

--- my-cli/src/prompts.js
+++ my-cli/src/prompts.js
@@ -15,3 +15,64 @@ export const promptListIds = async () => {
   return listCategoryItems(category);
 };
 await promptListIds()
+
+const orderQuestions = [
+    ...categoryQuestions,
+    {
+      type: "input",
+      name: "id",
+      message: "ID",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Name",
+    },
+    {
+      type: "input",
+      name: "amount",
+      message: "Amount",
+    },
+    {
+      type: "input",
+      name: "info",
+      message: "Info",
+    },
+  ];
+  
+  export const promptAddOrder = async () => {
+    const { category, id, name, amount, info } = await prompt(orderQuestions);
+    return add(category, id, name, amount, info);
+  };
+  
+  const updateQuestions = [
+    {
+      type: "input",
+      name: "id",
+      message: "ID",
+    },
+    {
+      type: "input",
+      name: "amount",
+      message: "Amount",
+    },
+  ];
+  
+  export const promptUpdate = async () => {
+    const { id, amount } = await prompt(updateQuestions);
+    return update(id, amount);
+  };
+  
+  const commandsList = ["add", "update", "list", "list by ID's", "help", "exit"];
+  const commandsQuestions = [
+    {
+      type: "autocomplete",
+      name: "command",
+      message: "Command",
+      choices: commandsList,
+    },
+  ];
+  export const promptCommand = async () => {
+    const { command } = await prompt(commandsQuestions);
+    return command;
+  };  
